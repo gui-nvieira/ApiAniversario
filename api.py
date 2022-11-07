6524daeb4ab199042a9ae6e1939dceee9707603a
Guilherme Vieira <81487846+gui-nvieira@users.noreply.github.com>
2022-11-07 17:03:10 -0300

    Building Heroku integration and configurating env
    Building Heroku integration and setting env

--- api.py
+++ api.py
@@ -1,16 +1,20 @@
 from flask import Flask, jsonify, request, json
 from flask_mysql_connector import MySQL
+from dotenv import load_dotenv
+import os
 import time
 
+load_dotenv()
 
 # Flask initializer
 app = Flask(__name__)
 
 # MySQL Config
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_PORT'] = 3307
-app.config['MYSQL_DATABASE'] = 'aniversario'
+app.config['MYSQL_USER'] = os.getenv('USERDB')
+app.config['MYSQL_PASSWORD'] = os.getenv('PASSDB')
+app.config['MYSQL_DATABASE'] = os.getenv('NAMEDB')
+app.config['MYSQL_HOST'] = os.getenv('HOSTDB')
+app.config['MYSQL_PORT'] = 3306
 mysql = MySQL(app)
 
 
@@ -24,7 +28,7 @@ def hello():
 @app.route('/user', methods=['GET'])
 def get_all_users():
     cur = mysql.new_cursor(dictionary=True)
-    cur.execute('select name, email, role from aniversario.usuarios;')
+    cur.execute('select name, email, role from usuarios;')
     output = cur.fetchall()
     return jsonify(output)
 
@@ -34,7 +38,7 @@ def get_all_users():
 def get_one_user(user_id):
     cur = mysql.new_cursor(dictionary=True)
     cur.execute(
-        'SELECT name, email, role from aniversario.usuarios WHERE user_id = %s ;', user_id)
+        'SELECT name, email, role from usuarios WHERE user_id = {} ;'.format(user_id))
     output = cur.fetchall()
     return jsonify(output)
 
@@ -48,44 +52,44 @@ def create_user():
     email = userData['Email']
     cur = mysql.new_cursor()
     cur.execute(
-        '''INSERT INTO aniversario.usuarios (`Name`,`Email`, `Signup_date`, `Last_updated`) 
+        '''INSERT INTO usuarios (`Name`,`Email`, `Signup_date`, `Last_updated`) 
         VALUES ( \'{}\', \'{}\', \'{}\', \'{}\' );'''.format(name, email, timeData, timeData))
     mysql.connection.commit()
     return jsonify({'msg': 'User registered'})
 
 
 # Api to confirm a user
-@ app.route('/user/confirm/<user_id>', methods=['PUT'])
+@app.route('/user/confirm/<user_id>', methods=['PUT'])
 def confirm_user(user_id):
     timeData = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     cur = mysql.new_cursor()
     cur.execute(
-        '''UPDATE aniversario.usuarios SET `Confirm` = 1, 
+        '''UPDATE usuarios SET `Confirm` = 1, 
         `Last_updated` = \'{}\' WHERE user_id = {};'''.format(timeData, user_id))
     mysql.connection.commit()
     return jsonify({'msg': 'User confirmed'})
 
 
 # Api to cancel a user
-@ app.route('/user/cancel/<user_id>', methods=['PUT'])
+@app.route('/user/cancel/<user_id>', methods=['PUT'])
 def cancel_user(user_id):
     timeData = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     cur = mysql.new_cursor()
     cur.execute(
-        '''UPDATE aniversario.usuarios SET `Confirm` = 1, 
+        '''UPDATE usuarios SET `Confirm` = 0, 
         `Last_updated` = \'{}\' WHERE user_id = {};'''.format(timeData, user_id))
     mysql.connection.commit()
     return jsonify({'msg': 'User cancelled'})
 
 
 # Api to delete a user
-@ app.route('/user/<user_id>', methods=['DELETE'])
+@app.route('/user/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
     cur = mysql.new_cursor()
     cur.execute(
-        '''DELETE FROM aniversario.usuarios WHERE user_id = {};'''.format(user_id))
+        '''DELETE FROM usuarios WHERE user_id = {};'''.format(user_id))
     mysql.connection.commit()
-    return jsonify({'msg': 'User cancelled'})
+    return jsonify({'msg': 'User deleted'})
 
 
 # Api initializer
